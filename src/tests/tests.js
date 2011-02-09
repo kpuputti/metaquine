cf0d22dfb963c540fbb20de75e68f5f004520416
Kimmo Puputti <kpuputti@gmail.com>
2011-02-09 21:32:40 +0200

    Added tests for provided procedures.

--- src/tests/tests.js
+++ src/tests/tests.js
@@ -10,4 +10,25 @@ $(function () {
         equals(typeof Metaquine, 'function', 'Metaquine function should be provided.');
     });
 
+    test('Provided procedures.', function () {
+        var meta, procedures, i, procedure, len;
+        meta = new Metaquine();
+        procedures = ['+', '-', '*', '/'];
+        expect(procedures.length + 1);
+
+        for (i = 0; i < procedures.length; i += 1) {
+            equals(typeof meta.procedures[procedures[i]], 'function',
+                   'Function ' + procedures[i] + ' should be provided.');
+        }
+
+        len = 0;
+        for (procedure in meta.procedures) {
+            if (meta.procedures.hasOwnProperty(procedure)) {
+                len += 1;
+            }
+        }
+        equals(len, procedures.length,
+               'All the tested and only the tested procedures should be provided.');
+    });
+
 });
